5d0faf2fb731f39bed231e1ee946cb7efe2414f1
AlleyNawaz <AliPythonDev@gmail.com>
2026-03-15 13:09:48 +0500

    Added new Speakers

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -3,6 +3,10 @@ import { Speaker } from "@/types";
 // GDE Images
 import aashiImg from "@/images/gde/aashi.jpeg";
 import ashmiImg from "@/images/gde/ashmi.jpeg";
+import connieImg from "@/images/gde/connie.jpeg";
+import damianImg from "@/images/gde/damian.png";
+import eyaImg from "@/images/gde/eya.png";
+import georgiosImg from "@/images/gde/georgios.jpeg";
 import imranImg from "@/images/gde/imran.jpeg";
 import kartikeyImg from "@/images/gde/kartikey.jpeg";
 import karthikImg from "@/images/gde/karthik.jpeg";
@@ -14,6 +18,7 @@ import pedroImg from "@/images/gde/pedro.jpeg";
 import radostinImg from "@/images/gde/radostin.jpeg";
 import sonuImg from "@/images/gde/sonu.png";
 import suesiImg from "@/images/gde/suesi.jpeg";
+import tomekImg from "@/images/gde/tomek.png";
 import ritwikImg from "@/images/gde/ritwik.jpeg";
 import abdurImg from "@/images/gde/abdur.jpeg";
 
@@ -31,12 +36,13 @@ import mujeebImg from "@/images/industryexperts/mujeeb.jpeg";
 import mugheesImg from "@/images/industryexperts/mughees.jpeg";
 import samiImg from "@/images/industryexperts/sami.png";
 import syedaImg from "@/images/industryexperts/syeda.jpeg";
+import uroojImg from "@/images/industryexperts/urooj.png";
 
 export const gdes: Speaker[] = [
     {
         id: "aashi-dutt",
         name: "Aashi Dutt",
-        role: "GDE AI",
+        role: "GDE in AI",
         image: aashiImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/aashi-dutt/" }
@@ -44,44 +50,71 @@ export const gdes: Speaker[] = [
     {
         id: "ashmi-banerjee",
         name: "Ashmi Banerjee",
-        role: "GDE AI",
-        company: "Google Cloud, AI",
+        role: "GDE in Google Cloud & AI",
         image: ashmiImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/ashmi-banerjee/" }
     },
     {
+        id: "connie-leung",
+        name: "Connie Leung",
+        role: "GDE in AI, Angular & Web Technologies",
+        image: connieImg.src,
+        isGDE: true,
+        socials: { linkedin: "https://www.linkedin.com/in/connieleung107/" }
+    },
+    {
+        id: "damian-sztankowski",
+        name: "Damian Sztankowski",
+        role: "GDE in Google Cloud",
+        image: damianImg.src,
+        isGDE: true,
+        socials: { linkedin: "https://www.linkedin.com/in/damian-sztankowski" }
+    },
+    {
+        id: "eya-laouini",
+        name: "Eya Laouini",
+        role: "GDE in Google Cloud & AI",
+        image: eyaImg.src,
+        isGDE: true,
+        socials: { linkedin: "https://www.linkedin.com/in/eya-laouini/" }
+    },
+    {
+        id: "georgios-soloupis",
+        name: "Georgios Soloupis",
+        role: "GDE in AI & Android",
+        image: georgiosImg.src,
+        isGDE: true,
+        socials: { linkedin: "https://www.linkedin.com/in/george-soloupis/" }
+    },
+    {
         id: "imran-us-salam",
         name: "Imran us Salam",
-        role: "GDE AI",
-        company: "Google Cloud, AI",
+        role: "GDE in Google Cloud & AI",
         image: imranImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/imran-us-salam-mian-54a48897/" }
     },
     {
-        id: "kartikey-rawat",
-        name: "Kartikey Rawat",
-        role: "GDE AI",
-        company: "Google Cloud, AI",
-        image: kartikeyImg.src,
-        isGDE: true,
-        socials: { linkedin: "https://www.linkedin.com/in/carrycooldude/" }
-    },
-    {
         id: "karthik-muthuswamy",
         name: "Karthik Muthuswamy",
-        role: "GDE AI",
-        company: "Google Cloud, AI",
+        role: "GDE in Google Cloud & AI",
         image: karthikImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/karthikmswamy/" }
     },
     {
+        id: "kartikey-rawat",
+        name: "Kartikey Rawat",
+        role: "GDE in Google Cloud & AI",
+        image: kartikeyImg.src,
+        isGDE: true,
+        socials: { linkedin: "https://www.linkedin.com/in/carrycooldude/" }
+    },
+    {
         id: "kshitiz-rimal",
         name: "Kshitiz Rimal",
-        role: "GDE AI",
-        company: "Google Cloud, AI",
+        role: "GDE in Google Cloud & AI",
         image: kshitizImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/kshitiz-rimal/" }
@@ -89,7 +122,7 @@ export const gdes: Speaker[] = [
     {
         id: "mashhood-rastgar",
         name: "Mashhood Rastgar",
-        role: "GDE Angular, AI & Web",
+        role: "GDE in Angular, AI & Web Technologies",
         image: mashhoodImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/mashhoodr/" }
@@ -97,7 +130,7 @@ export const gdes: Speaker[] = [
     {
         id: "muhammad-ahsan-ayaz",
         name: "Muhammad Ahsan Ayaz",
-        role: "GDE Angular & AI",
+        role: "GDE in Angular & AI",
         image: ahsanImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/ahsanayaz/" }
@@ -105,7 +138,7 @@ export const gdes: Speaker[] = [
     {
         id: "nitin-tiwari",
         name: "Nitin Tiwari",
-        role: "GDE AI",
+        role: "GDE in AI",
         image: nitinImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/tiwari-nitin/" }
@@ -113,8 +146,7 @@ export const gdes: Speaker[] = [
     {
         id: "pedro-gabriel",
         name: "Pedro Gabriel Lourenço",
-        role: "GDE AI",
-        company: "Google Cloud, AI",
+        role: "GDE in Google Cloud & AI",
         image: pedroImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/pedrogengo/" }
@@ -122,8 +154,7 @@ export const gdes: Speaker[] = [
     {
         id: "radostin-cholakov",
         name: "Radostin Cholakov",
-        role: "GDE AI",
-        company: "Google Cloud, AI",
+        role: "GDE in Google Cloud & AI",
         image: radostinImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/radostin-cholakov-bb4422146/" }
@@ -131,7 +162,7 @@ export const gdes: Speaker[] = [
     {
         id: "sonu-kapoor",
         name: "Sonu Kapoor",
-        role: "GDE Angular",
+        role: "GDE in Angular",
         image: sonuImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/sonu-kapoor/" }
@@ -139,12 +170,19 @@ export const gdes: Speaker[] = [
     {
         id: "suesi-tran",
         name: "Suesi Tran",
-        role: "GDE Flutter",
-        company: "Dart, Firebase, Flutter",
+        role: "GDE in Flutter, Dart & Firebase",
         image: suesiImg.src,
         isGDE: true,
         socials: { linkedin: "https://www.linkedin.com/in/suesitran/" }
     },
+    {
+        id: "tomek-porozynski",
+        name: "Tomek Porozynski",
+        role: "GDE in Google Cloud & AI",
+        image: tomekImg.src,
+        isGDE: true,
+        socials: { linkedin: "https://www.linkedin.com/in/tomaszporozynski/" }
+    }
 ];
 
 export const googleSpeakers: Speaker[] = [
@@ -270,6 +308,14 @@ export const industryExperts: Speaker[] = [
         company: "Google Developer Groups on Campus CUI",
         image: syedaImg.src,
         socials: { linkedin: "https://www.linkedin.com/in/syeda-aiman-mumtaz-sherazi-b32a0b280/" }
+    },
+    {
+        id: "urooj-fatima",
+        name: "Urooj Fatima",
+        role: "Instructor & Research Scientist in Machine Learning",
+        company: "Iqra University",
+        image: uroojImg.src,
+        socials: { linkedin: "https://www.linkedin.com/in/urooj-fatima-963394232/" }
     },
 ];
 
